cd6b13c467ab474332fd42ba8ed2527a7f1cf4cd
Dwi Kuntarto <dwikuntarto.work@gmail.com>
2021-10-01 14:54:55 +0700

    prepare for deployment

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -20,9 +20,12 @@ async function handler(req, res) {
         }
 
         let client;
+
+        const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.gjbiy.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
         try {
             // client = await MongoClient.connect('mongodb+srv://admin:admin@cluster0.mmceb.mongodb.net/my-site?retryWrites=true&w=majority');
-            client = await MongoClient.connect('mongodb+srv://admin:admin@cluster0.gjbiy.mongodb.net/my-site?retryWrites=true&w=majority');
+            client = await MongoClient.connect(connectionString);
         } catch (error) {
             res.status(500).json({ message: `Could not connect to database. ${error.message}` })
             return;
